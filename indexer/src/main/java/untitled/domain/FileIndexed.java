f2b6d21e1103195b139203bfbf4e5a510285571b
athcaga <121846555+athcaga@users.noreply.github.com>
2023-02-21 13:10:17 +0900

    commit

---
+++ indexer/src/main/java/untitled/domain/FileIndexed.java
@@ -0,0 +1,22 @@
+package untitled.domain;
+
+import java.util.*;
+import lombok.*;
+import untitled.domain.*;
+import untitled.infra.AbstractEvent;
+
+@Data
+@ToString
+public class FileIndexed extends AbstractEvent {
+
+    private Long id;
+    private Long field;
+
+    public FileIndexed(Index aggregate) {
+        super(aggregate);
+    }
+
+    public FileIndexed() {
+        super();
+    }
+}
